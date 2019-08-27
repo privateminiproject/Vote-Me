54204a4990e4a93dc950804830bdda6f1d095b75
rupeshjadhav <jadhavrupesh22@gmail.com>
2019-08-27 22:46:35 +0530

    Add Candidate is Done.!!!!

--- app/src/main/java/com/example/voteme/AddCandidate.java
+++ app/src/main/java/com/example/voteme/AddCandidate.java
@@ -4,7 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,69 +19,176 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class AddCandidate extends AppCompatActivity {
 
-    ProgressBar progressBar;
+//    ProgressBar progressBar;
     Button add;
     public EditText candidate_name,candidate_pass;
+    CircleImageView candidate_image;
+    private static final int PICK_IMAGE = 1;
+    private Uri imageUri;
+    CropImage.ActivityResult result;
+    private StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_candidate);
-        progressBar=findViewById(R.id.loginProgressBars);
-        progressBar.setVisibility(View.INVISIBLE);
+//        progressBar=findViewById(R.id.loginProgressBars);
+//        progressBar.setVisibility(View.INVISIBLE);
         add=findViewById(R.id.add_Candidate);
         candidate_name=findViewById(R.id.candidate_name);
         candidate_pass=findViewById(R.id.candidate_pass);
-        add.setOnClickListener(new View.OnClickListener() {
+        candidate_image=findViewById(R.id.candidate_image);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        candidate_image.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                progressBar.setVisibility(View.VISIBLE);
-                final String Candidate_Name = candidate_name.getText().toString();
-                final String Candidate_Desc = candidate_pass.getText().toString();
-                if (Candidate_Name.isEmpty()) {
-                    candidate_name.setError("Provide your Email first!");
-                    candidate_name.requestFocus();
-                } else if (Candidate_Desc.isEmpty()) {
-                    candidate_pass.setError("Set your password");
-                    candidate_pass.requestFocus();
-                } else if (Candidate_Name.isEmpty() && Candidate_Desc.isEmpty()) {
-                    Toast.makeText(AddCandidate.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
-                } else if (!(Candidate_Name.isEmpty() && Candidate_Desc.isEmpty())) {
-                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Candidate").push();
-                    Map<String, Object> map = new HashMap<>();
-                    map.put("id", databaseReference.getKey());
-                    map.put("Candidate_Name", Candidate_Name);
-                    map.put("Candidate_Description",Candidate_Desc);
-                    map.put("Candidate Image",Candidate_Desc);
-                    databaseReference.setValue(map).addOnSuccessListener(new OnSuccessListener<Void>() {
-                        @Override
-                        public void onSuccess(Void aVoid) {
-                            Intent in=new Intent(AddCandidate.this, Admin.class);
-                            startActivity(in);
-                            finish();
-                            progressBar.setVisibility(View.INVISIBLE);
-                        }
-                    });
 
 
-                } else {
-                    Toast.makeText(AddCandidate.this, "Error", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent();
+//                intent.setType("image/*");
+//                intent.setAction(Intent.ACTION_GET_CONTENT);
+//                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
 
-                }
-
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(AddCandidate.this);
 
             }
         });
 
 
 
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (imageUri!=null){
+//                    progressBar.setVisibility(View.VISIBLE);
+                    final String Candidate_Name = candidate_name.getText().toString();
+                    final String Candidate_Desc = candidate_pass.getText().toString();
+                    if (Candidate_Name.isEmpty()) {
+                        candidate_name.setError("Provide your Email first!");
+                        candidate_name.requestFocus();
+                    } else if (Candidate_Desc.isEmpty()) {
+                        candidate_pass.setError("Set your password");
+                        candidate_pass.requestFocus();
+                    } else if (Candidate_Name.isEmpty() && Candidate_Desc.isEmpty()) {
+                        Toast.makeText(AddCandidate.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
+                    } else if (!(Candidate_Name.isEmpty() && Candidate_Desc.isEmpty())) {
+
+                        final StorageReference filepath=mStorageRef.child("Images")
+                                .child(Candidate_Name+""+random()+".jpg");
+                        filepath.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                                if (task.isSuccessful()){
+
+                                    filepath.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                        @Override
+                                        public void onSuccess(Uri uri) {
+                                            Uri uris=uri;
+                                            String download_url=uri.toString();
+
+                                            Log.e("ImageLink",download_url);
+
+                                            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Candidate").push();
+                                            Map<String, Object> map = new HashMap<>();
+                                            map.put("id", databaseReference.getKey());
+                                            map.put("Candidate_Name", Candidate_Name);
+                                            map.put("Candidate_Description",Candidate_Desc);
+                                            map.put("Candidate Image",download_url);
+                                            databaseReference.setValue(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Intent in=new Intent(AddCandidate.this, Admin.class);
+                                                    startActivity(in);
+                                                    finish();
+//                                                    progressBar.setVisibility(View.INVISIBLE);
+                                                }
+                                            });
 
 
+
+                                        }
+                                    });
+
+
+
+                                }
+
+                            }
+                        });
+
+
+
+                    } else {
+                        Toast.makeText(AddCandidate.this, "Error", Toast.LENGTH_SHORT).show();
+
+                    }
+
+
+                }
+
+            }
+        });
     }
+
+    public static String random() {
+        Random generator = new Random();
+        StringBuilder randomStringBuilder = new StringBuilder();
+        int randomLength = generator.nextInt(10);
+        char tempChar;
+        for (int i = 0; i < randomLength; i++){
+            tempChar = (char) (generator.nextInt(96) + 32);
+            randomStringBuilder.append(tempChar);
+        }
+        return randomStringBuilder.toString();
+    }
+
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        result = CropImage.getActivityResult(data);
+//        if(requestCode == PICK_IMAGE){
+//            imageUri = data.getData();
+//            candidate_image.setImageURI(imageUri);
+//
+//        }
+//
+//    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                imageUri = result.getUri();
+                candidate_image.setImageURI(imageUri);
+                Log.e("Name",imageUri.toString());
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/voteme/MainActivity.java
+++ app/src/main/java/com/example/voteme/MainActivity.java
@@ -19,41 +19,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
 
-        Intent intent3 = getIntent();
-        String name = intent3.getStringExtra("Email");
-        Log.e("Current","Current User is = "+name);
 
-        if ( name== null) {
-            FirebaseAuth fAuth = FirebaseAuth.getInstance();
-            fAuth.signOut();
 
-            Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+//        mAuth = FirebaseAuth.getInstance();
+////        FirebaseUser currentUser = mAuth.getCurrentUser();
+////
+////        Intent intent3 = getIntent();
+////        String name = intent3.getStringExtra("Email");
+////        Log.e("Current","Current User is = "+name);
+////
+////        if ( name== null) {
+////            FirebaseAuth fAuth = FirebaseAuth.getInstance();
+////            fAuth.signOut();
+////
+            Intent intent=new Intent(MainActivity.this,AddCandidate.class);
             startActivity(intent);
             finish();
-        }
-
-        else{
-
-            if (name.equals("admin@gmail.com")){
-                Intent intent=new Intent(MainActivity.this,Admin.class);
-                startActivity(intent);
-                finish();
-            }
-            else{
-
-
-//                recyclerView = findViewById(R.id.list);
-//                linearLayoutManager = new LinearLayoutManager(this);
-//                recyclerView.setLayoutManager(linearLayoutManager);
-//                recyclerView.setHasFixedSize(true);
-//                fetch();
-            }
-
-
-        }
+////        }
+////
+////        else{
+////
+////            if (name.equals("admin@gmail.com")){
+////                Intent intent=new Intent(MainActivity.this,Admin.class);
+////                startActivity(intent);
+////                finish();
+////            }
+////            else{
+////
+////
+//////                recyclerView = findViewById(R.id.list);
+//////                linearLayoutManager = new LinearLayoutManager(this);
+//////                recyclerView.setLayoutManager(linearLayoutManager);
+//////                recyclerView.setHasFixedSize(true);
+//////                fetch();
+////            }
+////
+////
+////        }
 
 
     }
