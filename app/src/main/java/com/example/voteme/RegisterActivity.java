fe7347de60c41d27b79327be100bec2a65118b89
rupeshjadhav <jadhavrupesh22@gmail.com>
2019-08-27 19:43:40 +0530

    done login....

--- app/src/main/java/com/example/voteme/RegisterActivity.java
+++ app/src/main/java/com/example/voteme/RegisterActivity.java
@@ -93,10 +93,10 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
         });
-
-
     }
 
     public void Login(View view) {
+        Intent intent=new Intent(RegisterActivity.this,LoginActivity.class);
+        startActivity(intent);
     }
 }
