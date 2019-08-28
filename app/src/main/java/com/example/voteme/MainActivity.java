923ec1154eee7c466b0a4a2e53bbbaff91969d54
rupeshjadhav <jadhavrupesh22@gmail.com>
2019-08-28 18:09:33 +0530

    Verify voter is voted or not

--- app/src/main/java/com/example/voteme/MainActivity.java
+++ app/src/main/java/com/example/voteme/MainActivity.java
@@ -21,8 +21,11 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayoutManager linearLayoutManager;
     private FirebaseAuth mAuth;
     String name;
+    String total;
+    String userEmail;
+    FirebaseUser currentUser;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
 
     @Override
@@ -41,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        userEmail = currentUser.getEmail();
         Intent intent3 = getIntent();
         name = intent3.getStringExtra("Email");
         Log.e("Current", "Current User is = " + name);
@@ -68,6 +77,30 @@ public class MainActivity extends AppCompatActivity {
 
 
         }
+
+
+        Query query= FirebaseDatabase.getInstance().
+                getReference("Voter Email-Id").orderByChild("voter_name").equalTo(userEmail);
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                total= dataSnapshot.getChildrenCount()+"";
+                if (total.equals("1")){
+                    Intent intent=new Intent(MainActivity.this,Done.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
 
     }
 
@@ -113,6 +146,12 @@ public class MainActivity extends AppCompatActivity {
                 holder.root.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        DatabaseReference dbref = database.getReference("Voter Email-Id");
+                        dbref.push().child("voter_name").setValue(name);
+                        Intent intent=new Intent(MainActivity.this,Done.class);
+                        startActivity(intent);
+                        finish();
+
                         Toast.makeText(MainActivity.this, model.getName(), Toast.LENGTH_SHORT).show();
                     }
                 });
